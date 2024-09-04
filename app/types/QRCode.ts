b9f9a24a71aa860ed32bdf9ea2f5ceb5cf99703a
Muhamad Risman <real.risman@gmail.com>
2024-09-04 21:01:38 +0700

    feat: add qrcode detail route

---
+++ app/types/QRCode.ts
@@ -0,0 +1,10 @@
+import type { QRCode } from "@prisma/client";
+
+export type GeneratedQRCode = Omit<QRCode, "createdAt"> & {
+  createdAt: string;
+  productTitle: string;
+  productAlt: string;
+  productImage: string;
+  destinationUrl: string;
+  image: string;
+};
